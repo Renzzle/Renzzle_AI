7710595c5491b6fb7e701d53a160111787edc2b6
tintin46 <tintin4601@gmail.com>
2024-08-01 17:11:38 +0900

    feat tree, node, search

--- test/demo.cpp
+++ test/demo.cpp
@@ -17,6 +17,7 @@ void printBoardPattern(Board& board, Piece p);
 void printBoard(Board& board);
 void printPatternCells(CellArray& cells, Piece p, Direction k);
 void abpDemo();
+void testGenKey();
 
 int main() {
     #ifdef _WIN32
@@ -25,7 +26,8 @@ int main() {
 
     //patternDemo();
     //getCandidatesDemo();
-    abpDemo();
+    // abpDemo();
+    testGenKey();
 
     return 0;
 }
@@ -169,10 +171,28 @@ Board getBoard(string moves) {
     return board;
 }
 
+void testGenKey(){
+
+    Board board;
+    Tree tree;
+    board = getBoard("    a14a15c15d14h9e4e9c12j4k7f10b5m11i5g11c7e12g8k11g9c4d11f7k10k4f4e10d5f12i7i11i4c2k8d12g2d8m12g7g13g6j13j8l13h5i14e5e14l11l5k9f3l10f5e7k5i10c5c13i2j9m2h14a10a4b12b7d13f8k13n5m8j3f9b4a8d15d10c10g14l9o12j2k6m3a7j15d7e13g1j7l2c9n12n15o8n13o2n6m1m15j11n10n7n2o6");
+    TEST_TIME_START();
+    for (int i = 0; i < 10000; i++)
+        tree.generateKey(board);
+    TEST_TIME_END("genKey : ");
+
+
+}
+
 void abpDemo() {
     Board board;
     Evaluator eval;
-    VCFSearch vcfSearcher;
+    VCFSearch vcfSearcher(board);
+
+    /*
+    a14a15c15d14h9e4e9c12j4k7f10b5m11i5g11c7e12g8k11g9c4d11f7k10k4f4e10d5f12i7i11i4c2k8d12g2d8m12g7g13g6j13j8l13h5i14e5e14l11l5k9f3l10f
+    5e7k5i10c5c13i2j9m2h14a10a4b12b7d13f8k13n5m8j3f9b4a8d15d10c10g14l9o12j2k6m3a7j15d7e13g1j7l2c9n12n15o8n13o2n6m1m15j11n10n7n2o6
+    */
 
     // #0: [9, E] (depth=4)
     // board.move(Pos(7, 7));
@@ -201,14 +221,14 @@ void abpDemo() {
     // board.move(Pos(BOARD_SIZE + 1 - 10, 7));
     // board.move(Pos(BOARD_SIZE + 1 - 10, 8));
     // board.move(Pos(BOARD_SIZE + 1 - 10, 9));
-    // board.move(Pos(BOARD_SIZE + 1 - 11, 9));
+    // board.move(Pos(BOARD_SIZE + `1 - 11, 9));
     // board.move(Pos(BOARD_SIZE + 1 - 12, 10));
     // board.move(Pos(BOARD_SIZE + 1 - 7, 8));
     // board.move(Pos(BOARD_SIZE + 1 - 8, 10));
     // board.move(Pos(BOARD_SIZE + 1 - 7, 7));
     // board.move(Pos(BOARD_SIZE + 1 - 7, 6));
 
-    //board = getBoard("h8h9i8g8i10j9i9i7j10k11h10k10j8k7g10f10g11f12g7f6f7f11");
+    // board = getBoard("h8h9i8g8i10j9i9i7j10k11h10k10j8k7g10f10g11f12g7f6f7f11");
     //board = getBoard("h8h9i8g8i10i9h11g12j9i11j11k10h7i7");
     board = getBoard("h8g9h9g10h10h6i8i9g11i10f13h12d10e11d9e9d8b10c9f8e8i13e7e13i6");
     //board = getBoard("h8h9h10g8f9i9j9g10i8k10g9f7e6f8g6f5f6h6e9d8e8e7e11f10c9d9d10b8c11e12d11f11d13d7");
@@ -228,6 +248,7 @@ void abpDemo() {
     TEST_PRINT(bestMove.getX() << (char)(bestMove.getY() + 64));
 
     vcfSearcher.printWinningPath();
+    cout << "Nodes explored: " << vcfSearcher.getNodesExplored() << endl;
 
     // for (searchDepth = 9; searchDepth <= 11; searchDepth++) {
     //     TEST_TIME_START();
