14fdd466415e61f79691e599a416fb7073fe2c1a
Sang-Min Kang <rdrniel12345@gmail.com>
2025-06-01 02:40:07 +0900

    feat: add logic that do not repeat searching /#32

--- tree/tree.h
+++ tree/tree.h
@@ -7,13 +7,18 @@
 #include <memory>
 
 struct Node {
+    enum class ValueType {
+        EXACT, LOWER_BOUND, UPPER_BOUND, UNKNOWN
+    };
+
     Board board;
     Value value;
-    Result result;
+    ValueType valueType;
+    int searchedDepth;
     unordered_map<size_t, Node*> childNodes;
     Pos bestMove;
 
-    Node(Board b) : board(b), value(INITIAL_VALUE), result(ONGOING) {}
+    Node(Board b) : board(b), value(INITIAL_VALUE), searchedDepth(0), valueType(ValueType::UNKNOWN) {}
 };
 
 class Tree {
