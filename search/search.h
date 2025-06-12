5653e728ac87c91f838472345fd14dadc01668c3
Sang-Min Kang <rdrniel12345@gmail.com>
2025-06-12 20:45:55 +0900

    refactor: clean code

--- search/search.h
+++ search/search.h
@@ -35,6 +35,8 @@ PRIVATE
     Value abp(int depth);
     Value evaluateNode(Evaluator& evaluator);
     MoveList getCandidates(Evaluator& evaluator, bool isMax);
+    bool cutOffSearchedNode(stack<ABPNode>& stk);
+    void searchNextNode(stack<ABPNode>& stk);
     void sortChildNodes(MoveList& moves, bool isTarget);
     void updateParent(stack<ABPNode>& stk, Value childValue, SearchMode childMode);
     bool isGameOver(Board& board);
@@ -55,113 +57,63 @@ Search::Search(Board& board, SearchMonitor& monitor) : treeManager(board), monit
     });
 }
 
-Value Search::abp(int depth) {
-    stack<ABPNode> stk;
-    stk.push(ABPNode{depth, true, Value(MIN_VALUE, Value::Type::UNKNOWN), Value(MAX_VALUE + 1, Value::Type::UNKNOWN), Value(), 0, {}, SearchMode::FULL_WINDOW});
+bool Search::cutOffSearchedNode(stack<ABPNode>& stk) {
+    ABPNode &cur = stk.top();
+    Node* currentNode = treeManager.getNode();
+    bool cut = false;
 
-    while (!stk.empty()) {
-        monitor.updateElapsedTime();
-        ABPNode &cur = stk.top();
-        Node* currentNode = treeManager.getNode();
+    if (currentNode->searchedDepth >= cur.depth && 
+        currentNode->value.getValue() != INITIAL_VALUE && 
+        currentNode->value.getType() != Value::Type::UNKNOWN && 
+        cur.childIdx == 0) {
+        Value cv = currentNode->value;
 
-        // if already searched
-        if (currentNode->searchedDepth >= cur.depth && 
-            currentNode->value.getValue() != INITIAL_VALUE && 
-            currentNode->value.getType() != Value::Type::UNKNOWN && 
-            cur.childIdx == 0) {
-            Value cv = currentNode->value;
-            bool cut = false;
-
-            if (currentNode->value.getType() == Value::Type::EXACT) {
-                cut = true;
-            } else if (currentNode->value.getType() == Value::Type::LOWER_BOUND) {
-                if (cv >= cur.beta) cut = true;
-            } else if (currentNode->value.getType() == Value::Type::UPPER_BOUND) {
-                if (cv <= cur.alpha) cut = true;
-            }
-
-            if (cut) {
-                treeManager.undo();
-                stk.pop();
-
-                if (!stk.empty()) {
-                    updateParent(stk, cv, cur.searchMode);
-                }
-
-                continue;
-            }
+        if (currentNode->value.getType() == Value::Type::EXACT) {
+            cut = true;
+        } else if (currentNode->value.getType() == Value::Type::LOWER_BOUND) {
+            if (cv >= cur.beta) cut = true;
+        } else if (currentNode->value.getType() == Value::Type::UPPER_BOUND) {
+            if (cv <= cur.alpha) cut = true;
         }
 
-        // calculate child nodes & initialize best value
-        if (cur.childMoves.empty()) { // when first visit
-            Evaluator evaluator(currentNode->board);
-            cur.childMoves = getCandidates(evaluator, cur.isMax);
-            cur.bestVal = cur.isMax ? Value(MIN_VALUE, Value::Type::UNKNOWN) : Value(MAX_VALUE + 1, Value::Type::UNKNOWN);
-        }
-        
-        // if current node is leaf node
-        if (cur.depth == 0 || 
-            isGameOver(currentNode->board) || 
-            cur.childMoves.empty()) {
-            // evaluate leaf node value
-            Evaluator evaluator(currentNode->board);
-            Value val = evaluateNode(evaluator);
-            if (!cur.isMax) val.invert();
-            currentNode->value = val;
-
+        if (cut) {
             treeManager.undo();
             stk.pop();
 
             if (!stk.empty()) {
-                updateParent(stk, val, cur.searchMode);
-            }
-
-            continue;
-        }
-
-        if (cur.childIdx < cur.childMoves.size()) {
-            Pos move = cur.childMoves[cur.childIdx++];
-            treeManager.move(move);
-
-            Value nextAlpha = cur.alpha;
-            Value nextBeta = cur.beta;
-            nextAlpha.decreaseResultDepth();
-            nextBeta.decreaseResultDepth();
-
-            if (cur.searchMode == SearchMode::NULL_WINDOW) { // null window search in progress
-                stk.push({cur.depth - 1, !cur.isMax, nextAlpha, nextBeta, Value(), 0, {}, SearchMode::NULL_WINDOW});
-            } else if (cur.childIdx > 1) { // start new null window search
-                if (cur.isMax) {
-                    nextBeta = nextAlpha;
-                    nextBeta += 1;
-                } else {
-                    nextAlpha = nextBeta;
-                    nextAlpha -= 1;
-                }
-                stk.push({cur.depth - 1, !cur.isMax, nextAlpha, nextBeta, Value(), 0, {}, SearchMode::NULL_WINDOW});
-            } else {
-                stk.push({cur.depth - 1, !cur.isMax, nextAlpha, nextBeta, Value(), 0, {}, SearchMode::FULL_WINDOW});
-            }
-            
-            monitor.incVisitCnt();
-        } else { // if search every child nodes
-            if (cur.bestVal.getType() == Value::Type::UNKNOWN) {
-                cur.bestVal = cur.isMax ? cur.alpha : cur.beta;
-                cur.bestVal.setType(cur.isMax ? Value::Type::UPPER_BOUND : Value::Type::LOWER_BOUND);
-            }
-            currentNode->searchedDepth = cur.depth;
-            currentNode->value = cur.bestVal;
-            
-            treeManager.undo();
-            stk.pop();
-
-            if (!stk.empty()) {
-                updateParent(stk, cur.bestVal, cur.searchMode);
+                updateParent(stk, cv, cur.searchMode);
             }
         }
     }
 
-    return treeManager.getNode()->value;
+    return cut;
+}
+
+void Search::searchNextNode(stack<ABPNode>& stk) {
+    ABPNode &cur = stk.top();
+
+    Pos move = cur.childMoves[cur.childIdx++];
+    treeManager.move(move);
+
+    Value nextAlpha = cur.alpha;
+    Value nextBeta = cur.beta;
+    nextAlpha.decreaseResultDepth();
+    nextBeta.decreaseResultDepth();
+
+    if (cur.searchMode == SearchMode::NULL_WINDOW) { // null window search in progress
+        stk.push({cur.depth - 1, !cur.isMax, nextAlpha, nextBeta, Value(), 0, {}, SearchMode::NULL_WINDOW});
+    } else if (cur.childIdx > 1) { // start new null window search
+        if (cur.isMax) {
+            nextBeta = nextAlpha;
+            nextBeta += 1;
+        } else {
+            nextAlpha = nextBeta;
+            nextAlpha -= 1;
+        }
+        stk.push({cur.depth - 1, !cur.isMax, nextAlpha, nextBeta, Value(), 0, {}, SearchMode::NULL_WINDOW});
+    } else {
+        stk.push({cur.depth - 1, !cur.isMax, nextAlpha, nextBeta, Value(), 0, {}, SearchMode::FULL_WINDOW});
+    }
 }
 
 void Search::updateParent(stack<ABPNode>& stk, Value childValue, SearchMode childMode) {
@@ -218,6 +170,68 @@ void Search::updateParent(stack<ABPNode>& stk, Value childValue, SearchMode chil
         treeManager.undo();
         stk.pop();
     }
+}
+
+Value Search::abp(int depth) {
+    stack<ABPNode> stk;
+    stk.push(ABPNode{depth, true, Value(MIN_VALUE, Value::Type::UNKNOWN), Value(MAX_VALUE + 1, Value::Type::UNKNOWN), Value(), 0, {}, SearchMode::FULL_WINDOW});
+
+    while (!stk.empty()) {
+        monitor.updateElapsedTime();
+        ABPNode &cur = stk.top();
+        Node* currentNode = treeManager.getNode();
+
+        // if already searched
+        if (cutOffSearchedNode(stk)) continue;
+
+        // calculate child nodes & initialize best value
+        if (cur.childMoves.empty()) { // when first visit
+            Evaluator evaluator(currentNode->board);
+            cur.childMoves = getCandidates(evaluator, cur.isMax);
+            cur.bestVal = cur.isMax ? Value(MIN_VALUE, Value::Type::UNKNOWN) : Value(MAX_VALUE + 1, Value::Type::UNKNOWN);
+        }
+        
+        // if current node is leaf node
+        if (cur.depth == 0 || 
+            isGameOver(currentNode->board) || 
+            cur.childMoves.empty()) {
+            // evaluate leaf node value
+            Evaluator evaluator(currentNode->board);
+            Value val = evaluateNode(evaluator);
+            if (!cur.isMax) val.invert();
+            currentNode->value = val;
+
+            treeManager.undo();
+            stk.pop();
+
+            if (!stk.empty()) {
+                updateParent(stk, val, cur.searchMode);
+            }
+
+            continue;
+        }
+
+        if (cur.childIdx < cur.childMoves.size()) {
+            searchNextNode(stk);
+            monitor.incVisitCnt();
+        } else { // if search every child nodes
+            if (cur.bestVal.getType() == Value::Type::UNKNOWN) {
+                cur.bestVal = cur.isMax ? cur.alpha : cur.beta;
+                cur.bestVal.setType(cur.isMax ? Value::Type::UPPER_BOUND : Value::Type::LOWER_BOUND);
+            }
+            currentNode->searchedDepth = cur.depth;
+            currentNode->value = cur.bestVal;
+            
+            treeManager.undo();
+            stk.pop();
+
+            if (!stk.empty()) {
+                updateParent(stk, cur.bestVal, cur.searchMode);
+            }
+        }
+    }
+
+    return treeManager.getNode()->value;
 }
 
 Value Search::evaluateNode(Evaluator& evaluator) {
--- test/unit_test/search_test.cpp
+++ test/unit_test/search_test.cpp
@@ -50,7 +50,6 @@ PUBLIC
             "h8i8i9i7j8j7j6k7k6i6h7h6i5g7f8h10i10d8f6",
             "h8h9f6g7e9g8f7h6f8f9h7d6",
             "h8h9g7i9g9i7g10i8i10h10e8f9d8j7",
-            "h8h9i8i9j9j8k7l8k8i10l7k6j6i7h6g6h7h5g8f9",
             "h8h9i8g8i10j8i6h7h6k7j6f7f8",
             "h8h9i9i8g10h10f7g7h6h7g5i7k7j6k6k8l7j9k10"
         };
